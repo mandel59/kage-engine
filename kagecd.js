33c82958ed459aa8c1214df4ba5e49b516810218
Ryusei Yamaguchi <mandel59@gmail.com>
2023-05-03 16:58:11 +0900

    Remove void modules

--- kagecd.js
+++
@@ -1,6 +0,0 @@
-const { cdDrawCurveU, cdDrawBezier, cdDrawCurve, cdDrawLine } = require("./kage");
-
-exports.cdDrawCurveU = cdDrawCurveU;
-exports.cdDrawBezier = cdDrawBezier;
-exports.cdDrawCurve = cdDrawCurve;
-exports.cdDrawLine = cdDrawLine;
--- kagedf.js
+++
@@ -1,3 +0,0 @@
-const { dfDrawFont } = require("./kage");
-
-exports.dfDrawFont = dfDrawFont;
